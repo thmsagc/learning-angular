4c393bbfb962f1a0b1f490f76cc534c5778aea89
thmsagc <thomaschaves@ci.ufpb.br>
2022-10-09 17:27:16 -0300

    Event Binding with Button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  enableNewMessage = false;
+  message: string = "Waiting for click!"
+
+  constructor() {
+    setTimeout(() => {
+      this.enableNewMessage = true;
+    }, 2000)
+  }
+
+  onCreateMessage() {
+    this.message = "A new message was created."
+  }
 
 }
